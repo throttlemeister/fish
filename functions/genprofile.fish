40d27b3729527940599cbbcab68b5c767949ee83
throttlemeister <throttlemeister@sport-touring.eu>
2021-05-04 15:34:37 +0200

    Make sure .config/fish/conf.d/omf.fish is excluded as the servers do not have "Oy My Fish" installed and avoid harmless but annoying errors...

--- functions/genprofile.fish
+++ functions/genprofile.fish
@@ -1,5 +1,5 @@
 # Defined in - @ line 1
 function genprofile
   /usr/bin/cowsay Creating the profile archive "profile_proper.tar.gz" with all the required files...
-  /usr/bin/tar cvfz profile_proper.tar.gz ansible .secret .bash* .inputrc .profile .ssh/ .config/
+  /usr/bin/tar cvfz profile_proper.tar.gz ansible .secret .bash* .inputrc .profile .ssh/ .config/ --exclude .config/fish/conf.d/omf.fish
 end
