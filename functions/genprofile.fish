8fc438a8a246dba353b0f9d6418bca5d89812a34
throttlemeister <throttlemeister@sport-touring.eu>
2021-05-04 15:38:04 +0200

    Fixed a small oopsie. :-)

--- functions/genprofile.fish
+++ functions/genprofile.fish
@@ -1,5 +1,5 @@
 # Defined in - @ line 1
 function genprofile
   /usr/bin/cowsay Creating the profile archive "profile_proper.tar.gz" with all the required files...
-  /usr/bin/tar cvfz profile_proper.tar.gz ansible .secret .bash* .inputrc .profile .ssh/ .config/ --exclude .config/fish/conf.d/omf.fish
+  /usr/bin/tar cvfz profile_proper.tar.gz --exclude=.config/fish/conf.d/omf.fish ansible .secret .bash* .inputrc .profile .ssh/ .config/
 end
