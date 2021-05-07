2999976931ef573d7789b451b8c1540a73e71a6f
throttlemeister <throttlemeister@sport-touring.eu>
2021-05-07 17:02:06 +0200

    Typos and clean up. Seperated profile generation for servers and for WSL as servers do not need the Ansible scripts.

--- functions/deploy.fish
+++ functions/deploy.fish
@@ -1,5 +1,5 @@
 # Defined in - @ line 1
 function deploy
-  /usr/bin/cowsay Deploying new server(s) using Ansible...
+  /usr/bin/cowsay "Deploying new server(s) using Ansible..."
   /usr/bin/ansible-playbook /home/throttlemeister/ansible/site.yml --tags setup $argv;
 end
--- functions/genprofile.fish
+++ functions/genprofile.fish
@@ -1,5 +1,6 @@
 # Defined in - @ line 1
 function genprofile
   /usr/bin/cowsay Creating the profile archive "profile_proper.tar.gz" with all the required files...
-  /usr/bin/tar cvfz profile_proper.tar.gz --exclude=.config/fish/conf.d/omf.fish ansible .secret .bash* .inputrc .profile .ssh/ .config/
+  /usr/bin/tar cvfz profile_proper.tar.gz  .secret .bash* .inputrc .profile .ssh/ .config/
+  /usr/bin/tar cvfz profile_wsl.tar.gz ansible .secret .bash* .inputrc .profile .ssh/ .config/
 end
