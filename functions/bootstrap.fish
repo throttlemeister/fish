9d9598cac1060af7e05fdee17bdd504260e1fe36
throttlemeister <throttlemeister@sport-touring.eu>
2021-05-10 16:41:56 +0200

    Fixed bugs

--- functions/bootstrap.fish
+++ functions/bootstrap.fish
@@ -1,5 +1,5 @@
 # Defined in - @ line 1
 function bootstrap
   /usr/bin/cowsay "Bootstrapping new server(s) using Ansible..."
-  /usr/bin/ansible-playbook $HOME/ansible/bootstrap.yml --extra-vars "bootstrap_host=$argv";
+  /usr/bin/ansible-playbook $HOME/ansible/bootstrap.yml -i $argv, --extra-vars "variable_host=$argv" -u root --ask-pass;
 end
