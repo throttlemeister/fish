150f93562a8618d50c7365b6592221ed99a4e3d4
throttlemeister <throttlemeister@sport-touring.eu>
2021-05-10 11:20:58 +0200

    Updated standard grep flags

--- functions/grep.fish
+++ functions/grep.fish
@@ -1,8 +1,11 @@
 # Defined in - @ line 1
+#
+# Let's check if we have ripgrep installed and if so
+# we use it, otherwise use standard grep with options
 function grep
   if test -e /usr/local/bin/rg
     /usr/local/bin/rg  $argv;
   else
-    command grep -n $argv;
+    command grep -n --color $argv;
   end
 end
