c38332f0a44aa9479703e35a97b3a0340ec58a7e
throttlemeister <throttlemeister@sport-touring.eu>
2021-05-10 16:25:10 +0200

    Changed function such that a hostname or IP address can be provided as a commandline parameter to bootstrap a server

--- functions/bootstrap.fish
+++ functions/bootstrap.fish
@@ -1,5 +1,5 @@
 # Defined in - @ line 1
 function bootstrap
   /usr/bin/cowsay "Bootstrapping new server(s) using Ansible..."
-  /usr/bin/ansible-playbook $HOME/ansible/bootstrap.yml $argv;
+  /usr/bin/ansible-playbook $HOME/ansible/bootstrap.yml --extra-vars "bootstrap_host=$argv";
 end
