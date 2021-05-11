8047d32b9f94062e5842131e7437d23be854de3f
throttlemeister <throttlemeister@sport-touring.eu>
2021-05-11 15:09:54 +0200

    Clean up some code and add comments

--- functions/fish_greeting.fish
+++ functions/fish_greeting.fish
@@ -1,17 +1,16 @@
 function fish_greeting
+    # Giving us a nice welcome message
     echo -e " Welcome to:"
     figlet (hostname)
     echo ""
-    #inxi && echo "" && inxi -w
     inxi -S && inxi -a
-    #weather almelo
-    echo ""
+    # Setting some variables to make sure the latest profile is available on WSL
     set HOMEDIR $HOME
     set ONEDRIVE "/mnt/c/Users/throttlemeister/OneDrive/profile"
     set FILE profile_wsl.tar.gz
-    
+    # Checking if we are on WSL and if so, set the latest profile configuration
     if test -n "$WSL_INTEROP" && cmp -s "$HOMEDIR/$FILE" "$ONEDRIVE/$FILE"
-        echo "Profile OK!"
+        echo ""
     else if test -n "$WSL_INTEROP"
         cd $HOMEDIR
         cp $ONEDRIVE/profile* .
@@ -24,6 +23,6 @@ function fish_greeting
         echo ""
         echo "Profile reloaded!"
     else
-        echo "Not on WSL"
+        echo ""
     end
 end
