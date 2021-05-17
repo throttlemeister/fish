428ad1e2d618da6b48c33c8600f7a045a2f6f3c7
throttlemeister <throttlemeister@sport-touring.eu>
2021-05-17 11:04:41 +0200

    Removed adding hosts on CLI as it wasn't working

--- functions/bootstrap.fish
+++ functions/bootstrap.fish
@@ -1,5 +1,5 @@
 # Defined in - @ line 1
 function bootstrap
   /usr/bin/cowsay "Bootstrapping new server(s) using Ansible..."
-  /usr/bin/ansible-playbook $HOME/ansible/bootstrap.yml -i $argv, --extra-vars "variable_host=$argv" -u root --ask-pass;
+  /usr/bin/ansible-playbook $HOME/ansible/bootstrap.yml -u root --ask-pass;
 end
