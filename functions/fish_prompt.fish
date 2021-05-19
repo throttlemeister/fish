ed055a0d82c548e3d297c6776f41c99fa9adb76d
throttlemeister <throttlemeister@sport-touring.eu>
2021-05-19 13:34:17 +0200

    Readability bug with error code in right prompt

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -67,7 +67,7 @@ function fish_right_prompt
   set -l st $status
 
   if [ $st != 0 ];
-    echo (set_color red) ↵ $st(set_color normal)
+    echo (set_color red) ↵ $st  (set_color normal)
   end
   set_color -o 666
   date '+ %T'
