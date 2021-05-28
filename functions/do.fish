be4d20c8935781aa52b0d0430db94d22d52fa95f
throttlemeister <throttlemeister@sport-touring.eu>
2021-05-28 11:41:00 +0200

    Added catch for invalid arguments passed on the commandline

--- functions/do.fish
+++ functions/do.fish
@@ -1,7 +1,7 @@
 # Defined in - @ line 1
 function do
   if not set -q argv[1]
-    echo "Use 'do help' to see how to use this."
+    /usr/bin/cowsay -f eyes "No argument given. Please type: 'do help' to see how to use this."
   else
     switch $argv
       case help
@@ -13,14 +13,16 @@ function do
         echo "Example: do update"
         echo ""
       case bootstrap
-        /usr/bin/cowsay "Bootstrapping new server(s) using Ansible..."
+        /usr/bin/cowsay -f eyes "Bootstrapping new server(s) using Ansible..."
         /usr/bin/ansible-playbook $HOME/ansible/bootstrap.yml -u root --ask-pass;
       case profile
-        /usr/bin/cowsay "Deploying profile configuration..."
+        /usr/bin/cowsay -f eyes "Deploying profile configuration..."
         /usr/bin/ansible-playbook $HOME/ansible/profile.yml;
-      case '*'
-        /usr/bin/cowsay "$argv"ing all servers using Ansible...
+      case setup update hardening inxi
+        /usr/bin/cowsay -f eyes "$argv"ing all servers using Ansible...
         /usr/bin/ansible-playbook $HOME/ansible/site.yml --tags $argv;
+      case '*'
+        /usr/bin/cowsay -f eyes "No valid argument provided. Please use 'do help' to see all options!"
     end
   end
 end
