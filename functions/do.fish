44ea38f51efd4f3e912a96641990ee71911884fc
throttlemeister <throttlemeister@sport-touring.eu>
2021-05-31 12:47:15 +0200

    Added user creation / deletion to do.fish and updated the help section.

--- functions/do.fish
+++ functions/do.fish
@@ -3,9 +3,10 @@ function do
   if not set -q argv[1]
     /usr/bin/cowsay -f eyes -W 50 "No argument given. Please type: 'do help' to see how to use this command."
   else
-    switch $argv
+    switch $argv[1]
       case help
-        echo "To set up a brand-new server, use 'do bootstrap'"
+        echo "To set up a brand-new server, use 'do bootstrap [server_name]'"
+        echo "To create or remove a regular user on a server, use 'do user'"
         echo "To deploy the user profile configuration, use 'do profile'"
         echo ""
         echo "Use do plus one of the following tags to execute tasks on servers:"
@@ -14,13 +15,16 @@ function do
         echo ""
       case bootstrap
         /usr/bin/cowsay -f eyes "Bootstrapping new server(s) using Ansible..."
-        /usr/bin/ansible-playbook $HOME/ansible/bootstrap.yml -u root --ask-pass;
+        /usr/bin/ansible-playbook $HOME/ansible/bootstrap.yml -u root --ask-pass -e bootstrap_host=$argv[2];
+      case user
+        /usr/bin/cowsay -f eyes "Adding or removing a user to a system using Ansible..."
+        /usr/bin/ansible-playbook $HOME/ansible/user.yml -e host_user=$argv[2]
       case profile
         /usr/bin/cowsay -f eyes "Deploying profile configuration..."
         /usr/bin/ansible-playbook $HOME/ansible/profile.yml;
       case setup update hardening inxi
         /usr/bin/cowsay -f eyes "$argv"ing all servers using Ansible...
-        /usr/bin/ansible-playbook $HOME/ansible/site.yml --tags $argv;
+        /usr/bin/ansible-playbook $HOME/ansible/site.yml --tags $argv[1];
       case '*'
         /usr/bin/cowsay -f eyes -W 50 "No valid argument provided. Please use 'do help' to see all options!"
     end
