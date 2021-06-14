d358d74627f53d387d882eabefcab9421d052010
throttlemeister <throttlemeister@sport-touring.eu>
2021-06-14 14:20:28 +0200

    Make sure we update profile for root when we sudo as well if we are on WSL. If we aren't, we don't.

--- functions/root.fish
+++ functions/root.fish
@@ -1,4 +1,7 @@
 # Defined in - @ line 1
 function root --wraps='sudo -i' --description 'alias root sudo -i'
-  sudo -i $argv;
+  if test -n "$WSL_INTEROP"
+    sudo WSL_INTEROP=$WSL_INTEROP -i $argv;
+  else
+    sudo -i $argv;
 end
