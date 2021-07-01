4b9b3e380cc25854dd2e1ce0ed10c6a06e025355
throttlemeister <throttlemeister@sport-touring.eu>
2021-07-01 12:58:13 +0200

    Fixed path bug in genprofile.fish

--- functions/genprofile.fish
+++ functions/genprofile.fish
@@ -3,6 +3,7 @@ function genprofile
   set ONEDIR "/mnt/c/Users/throttlemeister/OneDrive/profile"
   set LDIR $HOME
   set cowsay (command -s cowsay)
+  cd $HOME
   $cowsay Creating the profile archives for servers and WSL with all the required files...
   /usr/bin/tar cvfz $ONEDIR/profile_proper.tar.gz  .secret .bash* .inputrc .profile .ssh/ .config/
   /usr/bin/tar cvfz $LDIR/profile_proper.tar.gz  .secret .bash* .inputrc .profile .ssh/ .config/
