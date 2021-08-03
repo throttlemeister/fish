6694d1293816c094177bbffba27385aec56cf3a0
throttlemeister <throttlemeister@sport-touring.eu>
2021-08-03 11:34:45 +0200

    Pulled bootstrapping and user adding from do.fish as the multiple arguments made it fail. Created seperate scripts bootstrap.fish and user.fish to replace these.

--- functions/do.fish
+++ functions/do.fish
@@ -14,18 +14,12 @@ function do
         cat $HOME/ansible/site.yml | grep "# Tags"
         echo "Example: do update"
         echo ""
-      case bootstrap
-        $cowsay -f eyes "Bootstrapping new server(s) using Ansible..."
-        /usr/bin/ansible-playbook $HOME/ansible/bootstrap.yml -u root --ask-pass -e bootstrap_host=$argv[2];
-      case user
-        $cowsay -f eyes "Adding or removing a user to a system using Ansible..."
-        /usr/bin/ansible-playbook $HOME/ansible/user.yml -u throttlemeister -K -e host_user=$argv[2] -i $HOME/ansible/inventory.yml
       case profile
         $cowsay -f eyes "Deploying profile configuration..."
         /usr/bin/ansible-playbook $HOME/ansible/profile.yml -i $HOME/ansible/inventory.yml;
       case setup update hardening inxi
         $cowsay -f eyes "$argv"ing all servers using Ansible...
-        /usr/bin/ansible-playbook $HOME/ansible/site.yml -u throttlemeister -K --tags $argv[1] -i $HOME/ansible/inventory.yml;
+        /usr/bin/ansible-playbook $HOME/ansible/site.yml -u throttlemeister -K --tags $argv -i $HOME/ansible/inventory.yml;
       case '*'
         $cowsay -f eyes -W 50 "No valid argument provided. Please use 'do help' to see all options!"
     end
