a8b8541f98298f96688da1a62a7a02d5472fc8f5
throttlemeister <throttlemeister@sport-touring.eu>
2021-09-17 11:29:49 +0200

    Updated profile generation

--- functions/genprofile.fish
+++ functions/genprofile.fish
@@ -1,12 +1,17 @@
 # Defined in - @ line 1
 function genprofile
-  set ONEDIR "/mnt/c/Users/throttlemeister/OneDrive/profile"
+  if test -n "$WSL_INTEROP"
+    set ONEDIR "/mnt/c/Users/throttlemeister/OneDrive/profile"
+  else
+    set ONEDIR "/home/throttlemeister/OneDrive/profile"
+  end
   set LDIR $HOME
   set cowsay (command -s cowsay)
   cd $HOME
   $cowsay Creating the profile archives for servers and WSL with all the required files...
-  /usr/bin/tar cvfz $ONEDIR/profile_proper.tar.gz  .gitconfig .bash* .inputrc .profile .ssh/ .config/
-  /usr/bin/tar cvfz $LDIR/profile_proper.tar.gz  .gitconfig .bash* .inputrc .profile .ssh/ .config/
-  /usr/bin/tar cvfz $ONEDIR/profile_wsl.tar.gz ansible .gitconfig .bash* .inputrc .profile .ssh/ .config/
-  /usr/bin/tar cvfz $LDIR/profile_wsl.tar.gz ansible .gitconfig .bash* .inputrc .profile .ssh/ .config/
+  /usr/bin/tar cvfz $ONEDIR/profile_proper.tar.gz  .gitconfig .bash* .inputrc .profile .ssh/ .config/fish
+  /usr/bin/tar cvfz $LDIR/profile_proper.tar.gz  .gitconfig .bash* .inputrc .profile .ssh/ .config/fish
+  /usr/bin/tar cvfz $ONEDIR/profile_wsl.tar.gz ansible .gitconfig .bash* .inputrc .profile .ssh/ .config/fish
+  /usr/bin/tar cvfz $LDIR/profile_wsl.tar.gz ansible .gitconfig .bash* .inputrc .profile .ssh/ .config/fish
+  cp $ONEDIR/profile_proper.tar.gz $HOME/ansible/files/
 end
