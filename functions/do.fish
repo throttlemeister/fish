3d8d037198b4417c6ee4c384f8d93bd8401e1808
throttlemeister <throttlemeister@sport-touring.eu>
2021-11-09 09:38:42 +0100

    Added lolcat for lolz :)

--- functions/do.fish
+++ functions/do.fish
@@ -1,6 +1,6 @@
 # Defined in - @ line 1
 function do
-  set cowsay (command -s cowsay)
+  set cowsay (command -s cowsay) | lolcat
   set var1 $argv[1]
   set var2 $argv[2]
   if not set -q argv[1]
@@ -17,19 +17,19 @@ function do
         echo "Example: do update"
         echo ""
       case bootstrap
-        $cowsay -f eyes "Bootstrapping new server(s) using Ansible..."
+        $cowsay -f eyes "Bootstrapping new server(s) using Ansible..." | lolcat
         /usr/bin/ansible-playbook $HOME/ansible/bootstrap.yml -u root --ask-pass -e bootstrap_host=$var2 -i $HOME/ansible/inventory.yml
       case user
-        $cowsay -f eyes "Adding or removing a user to a system using Ansible..."
+        $cowsay -f eyes "Adding or removing a user to a system using Ansible..." | lolcat
         /usr/bin/ansible-playbook $HOME/ansible/user.yml -u throttlemeister -K -e host_user=$var2 -i $HOME/ansible/inventory.yml
       case profile
-        $cowsay -f eyes "Deploying profile configuration..."
+        $cowsay -f eyes "Deploying profile configuration..." | lolcat
         /usr/bin/ansible-playbook $HOME/ansible/profile.yml -i $HOME/ansible/inventory.yml;
       case setup update hardening inxi
-        $cowsay -f eyes "$argv"ing all servers using Ansible...
+        $cowsay -f eyes ""$argv"ing all servers using Ansible..." | lolcat
         /usr/bin/ansible-playbook $HOME/ansible/site.yml -u throttlemeister -K --tags $argv[1] -i $HOME/ansible/inventory.yml;
       case '*'
-        $cowsay -f eyes -W 50 "No valid argument provided. Please use 'do help' to see all options!"
+        $cowsay -f eyes -W 50 "No valid argument provided. Please use 'do help' to see all options!" | lolcat
     end
   end
 end
