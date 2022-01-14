ed4c80edcdf29c0d97d7f2e5acaa9c6187ff2aac
throttlemeister <throttlemeister@sport-touring.eu>
2022-01-14 09:54:41 +0100

    Added option to create full profile backup to NAS. That is, a profile backup that includes .config for all program settings

--- functions/genprofile.fish
+++ functions/genprofile.fish
@@ -7,17 +7,26 @@ function genprofile
     set ONEDIR "/home/throttlemeister/OneDrive/profile"
     #ln -s /home/throttlemeister/OneDrive/ansible $HOME/ansible
   end
+  set full $argv
   set LDIR $HOME
   set cowsay (command -s cowsay)
   cd $HOME
-  $cowsay Creating the profile archives for servers and WSL with all the required files...
-  /usr/bin/tar cvfz $ONEDIR/profile_proper.tar.gz  .gitconfig .bash* .inputrc .profile .ssh/ .config/fish
-  /usr/bin/tar cvfz $LDIR/profile_proper.tar.gz  .gitconfig .bash* .inputrc .profile .ssh/ .config/fish
-  #/usr/bin/tar cvfz $ONEDIR/profile_wsl.tar.gz ansible .gitconfig .bash* .inputrc .profile .ssh/ .config/fish
-  #/usr/bin/tar cvfz $LDIR/profile_wsl.tar.gz ansible .gitconfig .bash* .inputrc .profile .ssh/ .config/fish
-  cp $ONEDIR/profile_proper.tar.gz $HOME/ansible/files/
-  cd $HOME/ansible
-  git commit --all -m "Updating profile"
-  git push
-  cd $HOME
+  if not set -q argv[1]
+    $cowsay Creating the profile archives for servers and WSL with all the required files...
+    /usr/bin/tar cvfz $ONEDIR/profile_proper.tar.gz  .gitconfig .bash* .inputrc .profile .ssh/ .config/fish
+    /usr/bin/tar cvfz $LDIR/profile_proper.tar.gz  .gitconfig .bash* .inputrc .profile .ssh/ .config/fish
+    #/usr/bin/tar cvfz $ONEDIR/profile_wsl.tar.gz ansible .gitconfig .bash* .inputrc .profile .ssh/ .config/fish
+    #/usr/bin/tar cvfz $LDIR/profile_wsl.tar.gz ansible .gitconfig .bash* .inputrc .profile .ssh/ .config/fish
+    cp $ONEDIR/profile_proper.tar.gz $HOME/ansible/files/
+    cd $HOME/ansible
+    git commit --all -m "Updating profile"
+    git push
+    cd $HOME
+  else
+    switch $full
+      case full
+        $cowsay Creating full profile archive...
+        /usr/bin/tar cvfz $HOME/nas/iscsi/profile_full.tar.gz --exclude-tag=no_backup ~/.config .gitconfig .bash* .inputrc .profile .ssh/
+    end
+  end
 end
