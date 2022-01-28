54f44f1031d70d1383717a8cd56324af49c54372
throttlemeister <throttlemeister@sport-touring.eu>
2022-01-28 11:04:49 +0100

    Cleanup

--- functions/genprofile.fish
+++ functions/genprofile.fish
@@ -7,12 +7,8 @@ function genprofile
   cd $HOME
   if not set -q argv[1]
     $cowsay Creating the profile archives for servers and WSL with all the required files...
-    #/usr/bin/tar cvfz $ONEDIR/profile_proper.tar.gz  .gitconfig .alacritty.yml .bash* .inputrc .profile .ssh/ .config/fish
-    #/usr/bin/tar cvfz $LDIR/profile_proper.tar.gz  .gitconfig .alacritty.yml .bash* .inputrc .profile .ssh/ .config/fish
     /usr/bin/tar cvfz $PROFILEDIR/profile_proper.tar.gz  .gitconfig .alacritty.yml .bash* .inputrc .profile .conkyrc .ssh/ .config/fish
     /usr/bin/tar cvfz $LDIR/profile_proper.tar.gz  .gitconfig .alacritty.yml .bash* .inputrc .profile .conkyrc .ssh/ .config/fish
-    #/usr/bin/tar cvfz $ONEDIR/profile_wsl.tar.gz ansible .gitconfig .bash* .inputrc .profile .ssh/ .config/fish
-    #/usr/bin/tar cvfz $LDIR/profile_wsl.tar.gz ansible .gitconfig .bash* .inputrc .profile .ssh/ .config/fish
     cd $HOME/ansible
     git commit --all -m "Updating profile"
     git push
