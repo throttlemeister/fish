ffa58c8069250901ad3076f967f3d7e23a75bbb5
throttlemeister <throttlemeister@sport-touring.eu>
2022-01-29 18:51:34 +0100

    Added some comments to make it clear what we are doing

--- functions/fish_greeting.fish
+++ functions/fish_greeting.fish
@@ -5,23 +5,29 @@ function fish_greeting
     echo ""
     inxi -S && inxi
     cd
-    # First make sure we have the latest version of code from git
+    # First make sure we have the latest version of code from git. If it's 
+    # not there, we clone the repo.
     if test -d $HOME/.config/fish/.git
       cd $HOME/.config/fish; git pull 1>/dev/null; cd
     else
       cd $HOME/.config
       git clone https://github.com/throttlemeister/fish.git
     end
+    # We do that for the Ansible directory too
     if test -d $HOME/ansible/.git
       cd $HOME/ansible; git pull 1>/dev/null; cd
     else
       cd $HOME
       git clone https://github.com/throttlemeister/ansible.git
     end
+    # GitHub way of authenticating changed, so we do not use this anymore.
+    # Housekeeping.
     if test -f "$HOME/.secret"
         echo "Deleting obsolete GitHub credentials"
         rm $HOME/.secret
     end
+    # If we are on an Apple, and there is a file called ''.apple', we swap the 
+    # super/WIN key and ALT key around
     if test -f "$HOME/.apple"
         echo "Swapping ALT and Super keys around"
         echo 1|sudo tee /sys/module/hid_apple/parameters/swap_opt_cmd
