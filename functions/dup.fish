208b2bfc733d972f4c332d9ff8e291f115740f54
throttlemeister <throttlemeister@sport-touring.eu>
2023-09-26 15:19:50 +0200

    Update, providing message as to why input is needed and making it red

--- functions/dup.fish
+++ functions/dup.fish
@@ -1,5 +1,6 @@
 function dup --wraps='sudo zypper'
     sudo zypper dup -y $argv;
+    set_color red; echo "Do you want to reboot?"; set_color normal
     if read_confirm = 0
 	    sudo shutdown -R now
     else
