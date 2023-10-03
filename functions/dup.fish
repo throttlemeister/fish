aa0783daea8d5ff658a54dc40ecf3c4abb122b15
throttlemeister <throttlemeister@sport-touring.eu>
2023-10-03 12:55:24 +0200

    Maybe its better to first show what processes need to be restarted before we make a decision on a reboot? (:

--- functions/dup.fish
+++ functions/dup.fish
@@ -1,11 +1,11 @@
 function dup --wraps='sudo zypper'
     sudo zypper dup -y $argv;
+    sudo zypper ps -s
     set_color red; echo "Do you want to reboot?"; set_color normal
     if read_confirm = 0
 	    sudo shutdown -R now
     else
-	    echo "Exiting without reboot. Showing processes which should be restarted instead"
-      zypper ps -s
+	    echo "Exiting without reboot."
     end
   end
   
