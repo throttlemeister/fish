0f73728da22af1dbafa12307eea2d52fe784e3c7
throttlemeister <throttlemeister@sport-touring.eu>
2023-11-08 10:46:02 +0100

    Updated prompt symbols

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -12,7 +12,7 @@ function __user_host
   if [ (id -u) = "0" ];
     echo -n (set_color --bold yellow)\((set_color --bold red)$USER(set_color --bold yellow)🛡️(set_color --bold red)$fqdn(set_color --bold yellow)\) (set color normal)
   else
-    echo -n (set_color --bold blue)\((set_color --bold white)$USER(set_color --bold blue)🔓(set_color --bold white)$fqdn(set_color --bold blue)\) (set color normal)
+    echo -n (set_color --bold blue)\((set_color --bold white)$USER(set_color --bold blue)♐(set_color --bold white)$fqdn(set_color --bold blue)\) (set color normal)
   end
 end
 
