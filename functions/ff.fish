bafdf32561321f52c0970c853aa33e0307e94df2
throttlemeister <throttlemeister@sport-touring.eu>
2024-03-25 23:31:56 +0100

    Added check if we are running Alacritty as it does not display images, so we fall back to ASCII logo for OpenSUSE.

--- functions/ff.fish
+++ functions/ff.fish
@@ -1,6 +1,10 @@
 function ff -d 'fastfetch shortcut'
   if type --quiet fastfetch
-    fastfetch -l ~/ansible/files/twgrey.png --logo-type iterm $argv
+    if test -n "$ALACRITTY_WINDOW_ID"
+      fastfetch -l opensuse $argv
+    else
+      fastfetch -l ~/ansible/files/twgrey.png --logo-type iterm $argv
+    end
   else
     command neofetch $argv
   end
