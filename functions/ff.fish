31ec3409713c24eb5a9778cd3b542edcffddf6ef
throttlemeister <throttlemeister@sport-touring.eu>
2024-03-26 19:05:56 +0100

    Align logo

--- functions/ff.fish
+++ functions/ff.fish
@@ -3,7 +3,7 @@ function ff -d 'fastfetch shortcut'
     if test -n "$ALACRITTY_WINDOW_ID"
       fastfetch -l opensuse $argv
     else
-      fastfetch -l ~/ansible/files/twgrey.png --logo-type iterm $argv
+      fastfetch -l ~/ansible/files/twgrey.png --logo-type iterm --logo-padding-top 2 $argv
     end
   else
     command neofetch $argv
